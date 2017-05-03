118b49d0d321d7266cf180dc2bcbb2b9d0cde74e
Marcel Jankrift <marcel@jankrift.de>
2017-05-03 12:23:54 +0200

    Some more explanation

--- example1.py
+++ example1.py
@@ -14,7 +14,9 @@ class PacMan():
             time.sleep(random.randint(1, 3))
             self.eat_dot()
 
-        # Create a counter for eat_dot() executions
+        # Create a counter for eat_dot() executions.
+        # It waits in the background for eat_dot() execution
+        # and checkes every time if number_dots is reached.
         """
         when eat_dot() == self.number_dots:
             self.next_level()
