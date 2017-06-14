4574b74b66ae141c884e7be6839954e2709dc830
Marcel Jankrift <marcel@jankrift.de>
2017-06-14 23:13:37 +0200

    Add example

---
+++ example.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+require 'pry'
+require_relative 'backtothefuture.rb'
+
+b = BackToTheFuture.new
+
+class TestClass
+  def method1
+    puts "Method1 running..."
+  end
+
+  def method2
+  end
+
+  def method3
+  end
+end
+
+test = TestClass.new
+
+# Example 1
+b.when [test, :method1] { puts "Method1 was called." }
+
+# Example 2
+b.when [test, :method1, {times: 5}] { puts "Method1 was called 5 times." }
+
+# Example 3
+b.when [test, :method1], [test, :method2] { puts "M1 and M2 were called." }
+
+# Example 4
+b.when [test, :method3, {times: 2}], :repeat { puts "bla" }
+
+# Example 5
+# b.in_last_seconds 5, test, :method1
+
+binding.pry
