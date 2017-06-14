feacd94aa8390c6a188141fc4a15f0e03a8a3769
Marcel Jankrift <marcel@jankrift.de>
2017-06-14 20:00:14 +0200

    stuff

--- backtothefuture.rb
+++ backtothefuture.rb
@@ -1,13 +1,114 @@
 #!/usr/bin/ruby
 
-class BackToTheFuture
-   def when object, method
+class WhenCase
+  @@times = 1
+  @@method = nil
+  @@object = nil
 
-     # TracePoint.trace(:call) do |t|
+  def initialize object, method, props
+    @@object = object
+    @@method = method
+    @@times = 1
+
+    if props.key?(:times)
+      @@times = props[:times]
+    end
+    puts @@object
+    puts @@method
+    puts @@times
+  end
+
+  def compare_method object, method
+    # if method == :method1
+    #   puts 123
+    #   puts object
+    #   puts @@object
+    # end
+      if method == :method1
+        puts object
+        puts @@object
+      end
+    if @@object == object && @@method == method
+      puts "-1"
+      @@times -= 1
+    end
+    @@times == 0
+  end
+end
+
+class WhenStatement
+  @@when_cases = []
+  def initialize(args, &block)
+    args.each do |a|
+      props = {}
+      if a.length == 3
+        props = a[2]
+      end
+      when_case = WhenCase.new(a[0], a[1], props)
+      @@when_cases.push(when_case)
+      @@block = block
+    end
+  end
+
+  def compare_methods object, method
+    return if @@when_cases.empty?
+    @@when_cases.delete_if do |c|
+      c.compare_method object, method
+    end
+    if @@when_cases.empty?
+      @@block.call
+    end
+  end
+end
+      
+      
+
+# class ObserveWhenStmt
+#   def init args
+#     args.each { |x| puts x }
+#     puts '123'
+#     yield
+#     puts '456'
+#   end
+# 
+#   def parse_methods args
+# 
+# end
+
+
+class BackToTheFuture
+    @@when_stmts = []
+   def initialize
      TracePoint.trace(:return) do |t|
-       if t.method_id == method && t.binding.receiver == object
-         yield
+       @@when_stmts.each do |w|
+         w.compare_methods t.binding.receiver, t.method_id
        end
      end
    end
+
+   def when(*args)
+     unless check_args(args)
+       puts "wrong args"
+       return
+     end
+     when_statement = WhenStatement.new(args) { yield }
+     @@when_stmts.push(when_statement)
+   end
+
+   def check_args(*args)
+     true
+   end
+
+
+     # when_stmt = ObserveWhenStmt.new
+     # when_stmt.init(args) { yield }
+     # @@when_stmts.push(when_stmt)
+
+
+     # TracePoint.trace(:call) do |t|
+     # TracePoint.trace(:return) do |t|
+     #   if t.method_id == method && t.binding.receiver == object
+     #     puts 'bla'
+     #   end
+     # end
 end
--- pacman.rb
+++ pacman.rb
@@ -19,10 +19,15 @@ test = TestClass.new
 test1 = TestClass.new
 
 # b.when ("TestClass.method1") { puts "blabla" }
-b.when test, :method1 { puts "blabla" }
-a = 7
+# b.when test, :method1 { puts "blabla" }
+# b.when [test, :method1, {times: 2}] do
+#   puts "XZVCZCV"
+# end
+b.when [test, :method1, {times: 2}], [test1, :method1] do
+  puts "XZVCZCVASDFASDFASD"
+end
 
-b.when test1, :method1 { puts "blablabla" }
+# b.when test1, :method1 { puts "blablabla" }
 
 # b.when ("TestClass.method2") do
 #   testvar = 12345
@@ -31,6 +36,7 @@ b.when test1, :method1 { puts "blablabla" }
 #   a = 55
 #   test.method1
 # end
+
 
 binding.pry
 # test.method1
